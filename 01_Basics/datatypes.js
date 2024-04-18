ac555a2d5a299b760f454da34dc582228ae626c8
Harshithcr <95340542+Harshithcr@users.noreply.github.com>
2024-04-18 07:31:36 +0000

    Adding files

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treats all the code as the newer version
+
+//alert(3+3) we cannont use it because we are using node js not browser
+
+
+console.log(3+      
+3);                 //code readability should be high
+
+console.log("Hello");
+
+let name="Harshith"; //String
+let age= 18; //number
+let isLoggedIn = true; //boolean
+
+//null stand alone value = represntation that is empty
+//undefined no value is assigned
+
+//object
+
+console.log(typeof(age));
+console.log(typeof(null));
+console.log(typeof(undefined));
---
+++ 01_Basics/variables.js
@@ -0,0 +1,12 @@
+const accId =6294;
+let email="harshith@gmail.com";
+// Prefer not use var because of functional scope
+var accPass="1234";
+
+// We can declare the variable without the datatype.But avoid doing it
+accCity="Bng";
+
+// The value will be undefined whe no value is initialized 
+let area;
+console.table([accId,email,accPass,accCity,area]);
+
